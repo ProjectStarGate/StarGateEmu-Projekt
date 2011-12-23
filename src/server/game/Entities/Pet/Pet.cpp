5399b816027e7d4fe90e15014e4bc0901d50177e
NeoMatrix <sharkipaust@web.de>
2011-12-23 14:34:29 +0100

    Core/Pet: Fix Water Elemental

--- src/server/game/Entities/Pet/Pet.cpp
+++ src/server/game/Entities/Pet/Pet.cpp
@@ -174,6 +174,12 @@ bool Pet::LoadPetFromDB(Player* owner, uint32 petentry, uint32 petnumber, bool c
             return false;
         }
     }
+	else if (pet_type == SUMMON_PET && summon_spell_id && !owner->HasSpell(summon_spell_id))
+    {
+    // pet (e.g.: Water Elemental) Mage
+        m_loading = false;
+        return false;
+    }
 
     uint32 pet_number = fields[0].GetUInt32();
 
