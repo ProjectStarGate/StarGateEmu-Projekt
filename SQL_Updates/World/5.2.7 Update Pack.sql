29406b2cedd0999abce95b9e0bd19bea4439bd9b
NeoMatrix <sharkipaust@web.de>
2012-01-12 12:14:37 +0100

    Core/SQL Update

--- SQL_Updates/World/5.2.7 Update Pack.sql
+++ SQL_Updates/World/5.2.7 Update Pack.sql
@@ -1,1 +1,9 @@
 UPDATE `creature_template` SET `modelid1`=11686, `ScriptName`='npc_shadowy_apparition' WHERE `entry`=46954;
+
+-- Atonement rank 1
+DELETE FROM `spell_proc_event` WHERE `entry` IN (14523);
+INSERT INTO `spell_proc_event` VALUES (14523, 0x00, 0x06, 0x00000080, 0x00000000, 0x00000000, 0x00010000, 0x00000000, 0, 100, 0);
+
+-- Atonement rank 2
+DELETE FROM `spell_proc_event` WHERE `entry` IN (81749);
+INSERT INTO `spell_proc_event` VALUES (81749, 0x00, 0x06, 0x00000080, 0x00000000, 0x00000000, 0x00010000, 0x00000000, 0, 100, 0);
