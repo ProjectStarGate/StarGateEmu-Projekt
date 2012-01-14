2190aa1d28857ab8983b70501f5a2ad8dc2f8f8e
NeoMatrix <sharkipaust@web.de>
2012-01-14 18:49:58 +0100

    Core/Spell: Fix Healing Rain.

--- src/server/game/Entities/DynamicObject/DynamicObject.h
+++ src/server/game/Entities/DynamicObject/DynamicObject.h
@@ -36,7 +36,7 @@ class DynamicObject : public WorldObject, public GridObject<DynamicObject>
         uint32 GetSpellId() const {  return GetUInt32Value(DYNAMICOBJECT_SPELLID); }
         uint64 GetCasterGUID() const { return GetUInt64Value(DYNAMICOBJECT_CASTER); }
         float GetRadius() const { return GetFloatValue(DYNAMICOBJECT_RADIUS); }
-
+		Aura* GetAura() { return m_aura; }
         void Say(int32 textId, uint32 language, uint64 TargetGuid) { MonsterSay(textId, language, TargetGuid); }
         void Yell(int32 textId, uint32 language, uint64 TargetGuid) { MonsterYell(textId, language, TargetGuid); }
         void TextEmote(int32 textId, uint64 TargetGuid) { MonsterTextEmote(textId, TargetGuid); }
